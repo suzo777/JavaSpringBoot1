e5af625ec8d8f471a71323ad7b0b34bb2def9412
suzo777 <73218354+suzo777@users.noreply.github.com>
2021-12-17 20:15:56 +0100

    comm-01

--- src/main/resources/data.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO user (email, name) VALUES
-('teszt.elek@data.hu', 'Teszt Elek'),
-('nagy.sandor@data.com', 'Nagy Sándor');
-
--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.accessingdatamysql;
+package com.example.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class AccessingDataMysqlApplicationTests {
+class DemoApplicationTests {
 
 	@Test
 	void contextLoads() {
