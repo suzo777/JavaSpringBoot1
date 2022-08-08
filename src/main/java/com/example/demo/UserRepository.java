217b221b5412c065235236190cd5d34decf4d6c4
suzo777 <73218354+suzo777@users.noreply.github.com>
2022-08-08 10:27:24 +0200

    torol1

--- src/main/java/com/example/demo/UserRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.demo;
-
-import org.springframework.data.repository.CrudRepository;
-import com.example.demo.User;
-public interface UserRepository extends CrudRepository<User, Integer> {
-}
